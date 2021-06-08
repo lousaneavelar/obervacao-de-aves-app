3ac795914f825222bbc88403a7a3e4ef6d18eecf
lousaneavelar <lousane.avelar@gmail.com>
2021-06-07 23:20:10 -0300

    Implementei a integração com o banco firebase, função de logar e cadastrar usuários. Implementei tratamento de exceções.

--- app/src/main/java/com/example/observacaodeaves/FormLogin.java
+++ app/src/main/java/com/example/observacaodeaves/FormLogin.java
@@ -1,14 +1,31 @@
 package com.example.observacaodeaves;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class FormLogin extends AppCompatActivity {
     private TextView text_telaCadastro;
+    private EditText edit_email, edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressBar;
+    String[] mensagens = {"Preencha todos os campos!"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +43,80 @@ public class FormLogin extends AppCompatActivity {
 
             }
         });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if(email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                    AutenticarUsuario(v);
+                }
+            }
+        });
+    }
+
+    private void AutenticarUsuario(View view){
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            TelaPerfil();
+
+                        }
+                    }, 3000);
+                }else{
+                    String erro;
+
+                    try {
+                        throw task.getException();
+                    } catch (Exception e){
+                        erro = "Erro ao logar usuário";
+                    }
+                    Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser usuarioAtual = FirebaseAuth.getInstance().getCurrentUser();
+        if(usuarioAtual != null){
+            TelaPerfil();
+        }
+    }
+
+    private  void TelaPerfil(){
+        Intent intent = new Intent(FormLogin.this, TelaPerfil.class);
+        startActivity(intent);
+        finish();
     }
 
     private void IniciarComponentes(){
         text_telaCadastro = findViewById(R.id.txtTelaCadastro);
+        edit_email = findViewById(R.id.edtEmail);
+        edit_senha = findViewById(R.id.edtSenha);
+        bt_entrar = findViewById(R.id.btnEntrar);
+        progressBar = findViewById(R.id.progressbar);
     }
 }
