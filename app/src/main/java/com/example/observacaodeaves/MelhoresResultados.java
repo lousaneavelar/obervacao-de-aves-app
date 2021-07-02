2396874f3e05838df266d171cb3e5cd2d87438b7
Vinicius Valente <viniciusorejana@yahoo.com.br>
2021-07-02 18:35:01 -0400

    melhores resultados e telaPesquisa

--- app/src/main/java/com/example/observacaodeaves/MelhoresResultados.java
+++ app/src/main/java/com/example/observacaodeaves/MelhoresResultados.java
@@ -7,12 +7,16 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 
+import java.util.ArrayList;
+
 public class MelhoresResultados extends AppCompatActivity {
     private BottomNavigationItemView bt_pesquisa, bt_principal, bt_perfil;
-
+    private ListView melhoresResultados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +25,34 @@ public class MelhoresResultados extends AppCompatActivity {
 
         getSupportActionBar().hide();
         IniciarComponentes();
+
+        ArrayAdapter adapter = new Adaptador(this, adicionarEspecies());
+        melhoresResultados.setAdapter(adapter);
+    }
+
+    private ArrayList<Especie> adicionarEspecies() {
+        ArrayList<Especie> especies = new ArrayList<Especie>();
+        Especie e = new Especie("Anu Branco", "Guira Guira", R.drawable.anubranco);
+        especies.add(e);
+        e = new Especie("Arara Canidé", "Ara ararauna", R.drawable.araracaninde);
+        especies.add(e);
+        e = new Especie("Rapazinho do Chaco", "Nystalus striatipectus", R.drawable.rapazinhodochaco);
+        especies.add(e);
+        e = new Especie("Tiriba fogo", "Pyrrhura devillei", R.drawable.tiribafoto);
+        especies.add(e);
+        e = new Especie("Tucanuçu", "Ramphastos toco", R.drawable.tucanucu);
+        especies.add(e);
+        e = new Especie("Príncipe", "Pyrocephalus", R.drawable.principe);
+        especies.add(e);
+        e = new Especie("Sanhaço", "Thraupis", R.drawable.sanhaco);
+        especies.add(e);
+
+        return especies;
+
     }
 
     private void IniciarComponentes(){
+        melhoresResultados = (ListView) findViewById(R.id.melhoresResultados);
         bt_perfil = findViewById(R.id.nav_perfil);
         bt_pesquisa = findViewById(R.id.nav_search);
         bt_principal = findViewById(R.id.nav_home);
